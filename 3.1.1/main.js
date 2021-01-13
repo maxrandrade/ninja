f98702c28d8f3f131df808290691869c784cb1eb
Max Andrade <maxreisandrade@gmail.com>
2021-01-13 12:20:04 -0300

    New function declaration asserts

--- 3.1.1/main.js
+++ 3.1.1/main.js
@@ -17,3 +17,34 @@ window.onload = function() {
   }) === text,
   'The useless function works! ' + text)
 }
+
+// Page 70
+
+function isNimble() { return true }
+
+assert(typeof window.isNimble === 'function', 'isNimble() defined')
+assert(isNimble.name === 'isNimble', 'isNimble() has a name')
+
+var canFly = function() { return true }
+assert(typeof window.canFly === 'function', 'canFly() defined')
+assert(canFly.name === '', 'canFly() has no name')
+console.log(window)
+
+window.isDeadly = function() { return true }
+assert(typeof window.isDeadly === 'function', 'isDeadly() defined')
+
+function outer(){
+  assert(typeof inner === 'function', 'inner() in scope before declaration')
+  function inner(){}
+  assert(typeof inner === 'function', 'inner() in scope after declaration')
+  assert(window.inner === undefined, 'inner() not in global scope')
+}
+outer()
+
+assert(window.inner === undefined, 'inner() still not in global scope')
+
+window.wieldsSword = function swingsSword() { return true }
+assert(window.wieldsSword.name === 'swingsSword', "wieldSword's real name is swingsSword")
+
+var ownTest = function () { return true }
+assert(window.ownTest.name === '', "ownTest's real name is namedFunction")
