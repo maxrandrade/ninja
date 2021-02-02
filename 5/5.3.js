305bbb204f1f815bb735d64f2bd727fd9c31be3d
Max Andrade <maxreisandrade@gmail.com>
2021-02-02 18:46:30 -0300

    5.3

---
+++ 5/5.3.js
@@ -0,0 +1,22 @@
+var outerValue = 'ninja'
+var later
+
+function outerFunction() {
+  var innerValue = 'samurai'
+
+  function innerFunction(paramValue) {
+    assert(outerValue, 'Inner can see the ninja')
+    assert(innerValue, 'Inner can see the samurai')
+    assert(paramValue, 'Inner can see the wakizashi')
+    assert(tooLate, 'Inner can see the ronin')
+  }
+
+  later = innerFunction
+}
+
+assert(!tooLate, 'Outer can\'t see the ronin.')
+
+var tooLate = 'ronin'
+
+outerFunction()
+later('wakizashi')
