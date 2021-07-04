eeb71ad6bf65b89095045b9da7f302c51d822497
Max Andrade <maxreisandrade@gmail.com>
2021-07-03 21:19:19 -0300

    refatorando bloco de testes

--- 3/3.2.js
+++ 3/3.2.js
@@ -1,59 +1,35 @@
-assert(true, '|----- BEFORE OUTER -----|')
-assert(typeof outer === 'function', 'outer() is in scope')
-assert(typeof inner === 'function', 'inner() is in scope')
-assert(typeof a === 'number', 'a is in scope')
-assert(typeof b === 'number', 'b is in scope')
-assert(typeof c === 'number', 'c is in scope')
+tests('|----- BEFORE OUTER -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
 
 function outer() {
-  assert(true, '|----- INSIDE OUTER, BEFORE a -----|')
-  assert(typeof outer === 'function', 'outer() is in scope')
-  assert(typeof inner === 'function', 'inner() is in scope')
-  assert(typeof a === 'number', 'a is in scope')
-  assert(typeof b === 'number', 'b is in scope')
-  assert(typeof c === 'number', 'c is in scope')
+  tests('|----- INSIDE OUTER, BEFORE a -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
+
   var a = 1
 
-  assert(true, '|----- INSIDE OUTER, AFTER a -----|')
-  assert(typeof outer === 'function', 'outer() is in scope')
-  assert(typeof inner === 'function', 'inner() is in scope')
-  assert(typeof a === 'number', 'a is in scope')
-  assert(typeof b === 'number', 'b is in scope')
-  assert(typeof c === 'number', 'c is in scope')
+  tests('|----- INSIDE OUTER, AFTER a -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
 
-  function inner(){ /* does nothing */}
+  function inner(){/* does nothing */}
+  
   var b = 2
 
-  assert(true, '|----- INSIDE OUTER, AFTER inner() AND b -----|')
-  assert(typeof outer === 'function', 'outer() is in scope')
-  assert(typeof inner === 'function', 'inner() is in scope')
-  assert(typeof a === 'number', 'a is in scope')
-  assert(typeof b === 'number', 'b is in scope')
-  assert(typeof c === 'number', 'c is in scope')
+  tests('|----- INSIDE OUTER, AFTER inner() AND b -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
 
   if (a === 1) {
     var c = 3
-    assert(true, '|----- INSIDE OUTER, INSIDE if -----|')
-    assert(typeof outer === 'function', 'outer() is in scope')
-    assert(typeof inner === 'function', 'inner() is in scope')
-    assert(typeof a === 'number', 'a is in scope')
-    assert(typeof b === 'number', 'b is in scope')
-    assert(typeof c === 'number', 'c is in scope')
+    tests('|----- INSIDE OUTER, INSIDE if -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
   }
 
-  assert(true, '|----- INSIDE OUTER, OUTSIDE if -----|')
-  assert(typeof outer === 'function', 'outer() is in scope')
-  assert(typeof inner === 'function', 'inner() is in scope')
-  assert(typeof a === 'number', 'a is in scope')
-  assert(typeof b === 'number', 'b is in scope')
-  assert(typeof c === 'number', 'c is in scope')
+  tests('|----- INSIDE OUTER, OUTSIDE if -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
 }
 
 outer()
 
-assert(true, '|----- AFTER OUTER -----|')
-  assert(typeof outer === 'function', 'outer() is in scope')
-  assert(typeof inner === 'function', 'inner() is in scope')
-  assert(typeof a === 'number', 'a is in scope')
-  assert(typeof b === 'number', 'b is in scope')
-  assert(typeof c === 'number', 'c is in scope')
+tests('|----- AFTER OUTER -----|', typeof outer, typeof inner, typeof a, typeof b, typeof c)
+
+function tests(message, outer, inner, a, b, c) {
+  assert(true, message)
+  assert(outer === 'function', 'outer() is in scope')
+  assert(inner === 'function', 'inner() is in scope')
+  assert(a === 'number', 'a is in scope')
+  assert(b === 'number', 'b is in scope')
+  assert(c === 'number', 'c is in scope')
+}
