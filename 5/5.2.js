8ece1c64b82f86dc2b1a02b1821bba449cc0684c
Max Andrade <maxreisandrade@gmail.com>
2021-07-20 14:00:50 -0300

    teste

--- 5/5.2.js
+++ 5/5.2.js
@@ -3,14 +3,16 @@ var later;
 
 function outerFunction() {
   var innerValue = 'samurai'
-
+  
   function innerFunction() {
-    assert(outerValue, 'I can see the ninja')
+    console.log(outerValue)
+    assert(outerValue == 'ninja', 'I can see the ninja')
     assert(innerValue, 'I can see the samurai')
   }
-
   later = innerFunction
 }
 
+
 outerFunction()
+outerValue = 'saaaa'
 later()
